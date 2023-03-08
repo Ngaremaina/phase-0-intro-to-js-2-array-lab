2cf0a84cc2e79e8fba605a3de38db006a4413555
Ngaremaina <ngaremaina4@gmail.com>
2023-03-08 16:53:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+
+}
+function destructivelyRemoveFirstCat(name){
+    return cats.shift();
+
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop();
+
+}
+function appendCat(name){
+    const copycats=[...cats];
+    copycats.push(name);
+    return copycats;
+
+}
+function prependCat(name){
+    const copycats = [...cats];
+    copycats.unshift(name);
+    return copycats;
+
+}
+function removeLastCat(name){
+    const copycats = [...cats];
+    copycats.pop();
+    return copycats;
+
+}
+function removeFirstCat(name){
+    const copycats = [...cats];
+    copycats.shift();
+    return copycats;
+
+}
